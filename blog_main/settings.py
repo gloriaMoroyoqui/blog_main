d49734656832170f668f0143512cd14f0b047ac4
gloriaMoroyoqui <gloriamoroyoqui8121@gmail.com>
2026-03-26 18:22:11 -0700

    hice cambios

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ljru!#jj=tl!7_r%cv(2uw$v(^6&-7yk&18o1ab51u^of)*!mf'
+SECRET_KEY = 'django-insecure-46rpz=s5j=@qmsi$q50*ln@!&7=c9n_kldo3etvcpzi2!$z!=k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -114,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
