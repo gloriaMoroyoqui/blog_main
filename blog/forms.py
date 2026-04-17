bbc9d51df8b2906bca8d9875b259a160c15e2fdc
gloriaMoroyoqui <gloriamoroyoqui8121@gmail.com>
2026-04-16 19:08:17 -0700

    hice cambios

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
-from .models import Comment 
+from .models import Comment
 
-class CommentForm(forms.Modelform):
+class CommentForm(forms.ModelForm):
     class Meta:
-        model = Comment 
-        fields = ['name','email','body']
+        model = Comment
+        fields = ['name', 'email', 'body']
