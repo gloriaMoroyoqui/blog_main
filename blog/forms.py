6e7e27fb98c2eb948edc8043a2b50f3783051174
gloriaMoroyoqui <gloriamoroyoqui8121@gmail.com>
2026-04-17 15:20:40 -0700

    puse info en forms

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,19 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class':'form-control',
+                'placeeholder':'tu nombre'
+            }),
+            'email': forms.Textarea(attrs={
+                 'class': 'form-control',
+                'rows':1,
+                'placeholder':'tu email'
+             }),
+             'body': forms.Textarea(attrs={
+                 'class':'form-control',
+                 'rows':5,
+                 'placeholder':'Escribe tu comentario'
+                 }),
+            }
