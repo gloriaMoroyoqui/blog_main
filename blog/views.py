64680b9182a8effe96f17ccf842bbbb563552deb
gloriaMoroyoqui <gloriamoroyoqui8121@gmail.com>
2026-04-16 18:57:09 -0700

    hice cambios

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, render
 
 from.models import Post, Category
+from .forms import CommentForm
 
 # Create your views here.
 def home(request):
